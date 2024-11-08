08f3249b563b0f223e97cc5b240869f4b54658bf
Sanjay <sanjaymishra.mymail@gmail.com>
2024-11-08 11:09:22 +0100

    Sample game application to test TCLTest framework
    U get 100$ incase the sum is >4 and product is >6

---
+++ game.tcl
@@ -0,0 +1,39 @@
+# game.tcl
+
+# Source the math.tcl file
+source math.tcl
+source code_coverage.tcl
+
+# Global variables to hold values of a and b
+set a 0
+set b 0
+
+# Procedure to set values of a and b
+proc set_values {x y} {
+    code_coverage set_values
+    global a b
+    set a $x
+    set b $y
+}
+
+# Reward function
+proc reward {} {
+    code_coverage reward
+    global a b
+
+    # Calculate the addition and multiplication
+    set sum [add $a $b]
+    set product [multiply $a $b]
+
+    # Check the conditions for reward
+    if {$sum > 4 && $product >= 6} {
+        puts "Great! You have won a reward."
+    return 100;
+    } else {
+        puts "Oh oh, better luck next time."
+    return 0;
+    }
+}
+
+set_values 2 3
+reward
